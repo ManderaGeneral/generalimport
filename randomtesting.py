500547e77695ce9e8edd4a1ac54d498575bff2f4
Mandera <rickard.abraham@gmail.com>
2022-10-25 22:42:33 +1000

    [CI SKIP] Simple comparison, could be an example

--- randomtesting.py
+++ randomtesting.py
@@ -1,23 +1,37 @@
-import importlib
-import sys
-import inspect
-from pprint import pprint
-import importlib
+
+from unittest import TestCase, skipIf
 
 
-# HERE ** Time for SO question!
+try:
+    import optional
+except ImportError:
+    optional = None
 
-# from generalimport import generalimport
-#
-# generalimport("hi")
-#
-# import hi
-#
-# print(hi)
+def func():
+    if optional is None:
+        raise ModuleNotFoundError("Missing dependency 'optional'.")
+    return optional.func()
+
+class Testing(TestCase):
+    @skipIf(optional is None)
+    def test_method(self):
+        self.assertEqual("foobar", optional.func())
 
 
 
+from generalimport import generalimport
+generalimport("optional")
 
+from unittest import TestCase
+import optional
+
+
+def func():
+    return optional.func()
+
+class Testing(TestCase):
+    def test_method(self):
+        self.assertEqual("foobar", optional.func())
 
 
 
